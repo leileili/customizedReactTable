ff4f05ddacf12bff7f3e282c7608c978b88324f3
leileili <32149236+leileili@users.noreply.github.com>
2019-01-02 15:36:21 -0800

    Update Footer.js

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -5,7 +5,7 @@ import '../../css/footer.css';
 
 class Footer extends React.Component {
 	render() {
-  	return (<div id="footer">Cisco Success Portal</div>)
+  	return (<div id="footer">Customized React Table</div>)
   }
 }
 
